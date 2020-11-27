e59886af769501af3e13cc5b0cb35e11ad484725
Jbeltrez <beltrezjohn@gmail.com>
2020-11-27 10:38:22 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  
   end
 
   def new
@@ -19,5 +20,24 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit 
+
+    @article = Article.find(params[:id])
+
+  end     
+
+  def update 
+    puts params.inspect 
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article) 
+    # raise params.inspect 
+  end 
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end
   # add edit and update methods here
 end
